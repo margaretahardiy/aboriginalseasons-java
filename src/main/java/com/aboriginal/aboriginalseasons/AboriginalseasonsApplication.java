6efaecc58da66ff7e32eb5308c30a9a57fd976c2
Margareta Hardiyanti <mhardiyanti@student.unimelb.edu.au>
2018-08-03 09:08:11 +1000

    Setup Project

---
+++ src/main/java/com/aboriginal/aboriginalseasons/AboriginalseasonsApplication.java
@@ -0,0 +1,12 @@
+package com.aboriginal.aboriginalseasons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboriginalseasonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboriginalseasonsApplication.class, args);
+	}
+}
